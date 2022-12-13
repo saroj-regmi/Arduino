3fa7b877cb33d7004494246caf84bb025809267b
itsmeisha <ishapun09@gmail.com>
2022-12-13 16:58:56 +0545

    Update: bt module btn press running servo done

--- servo_flag/servo_flag.ino
+++ servo_flag/servo_flag.ino
@@ -2,13 +2,31 @@
 
 // creating hamroserver object
 Servo hamroServo;
-
 int servoPin = 7; // servo pin
 
+// stores the command from the user
+char command = 0;
+
 void setup() {
+//for the bluetooth module
+Serial.begin(9600);
+
+
+
+//servo motor
 hamroServo.attach(servoPin);
 }
 
 void loop() {
-  hamroServo.write(720);
+  
+
+//checking it the bluetooth module is available
+if(Serial.available() > 0){
+  command = Serial.read(); // reading the input of the user
+  Serial.print(command);
+  if(command == '1'){
+     hamroServo.write(720);
+    } 
 }
+   
+ }
